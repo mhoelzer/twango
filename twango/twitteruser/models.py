5dc826eb195a9f3502076e351c75a37e7bc9489a
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-13 15:31:43 -0400

    updated model

--- twango/twitteruser/models.py
+++ twango/twitteruser/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class TwitterUser(models.Model):
     username = models.CharField(max_length=50)
+    display_name = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
