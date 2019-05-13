951e76244a43342751aa8e0c4732888479c39f4d
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-13 11:53:26 -0400

    added first model

---
+++ twango/twitteruser/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TwitterUser(models.Model):
+    username = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
