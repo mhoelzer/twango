d02cc56ac5fd7f3b1b173541bed8a33c90ce19ed
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-15 16:05:20 -0400

    can tweet and have it show up

--- twango/tweet/urls.py
+++ twango/tweet/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from twango.tweet.views import (twang_view)
+# from twango.tweet.views import (twang_creation_view)
+from twango.tweet.views import (twang_creation_view, twang_view)
 
 
 urlpatterns = [
-    path("twang/", twang_view),
+    path("twang/", twang_creation_view),
+    path("twangs/", twang_view)
+    # path("twang/<int:id>", twang_specific_view)
+    # make sure str woprks
 ]
--- twango/tweet/views.py
+++ twango/tweet/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 # from twango.twitteruser.models import TwitterUser
 from twango.tweet.models import Tweet
-from twango.tweet.forms import (TwangForm)
+from twango.tweet.forms import TwangForm
 from django.contrib.auth.decorators import login_required
 
 
 @login_required()
-def twang_view(request):
+def twang_creation_view(request):
     html = "../templates/generic.html"
     header = "Twang awayng!"
     form = None
@@ -17,12 +17,32 @@ def twang_view(request):
             data = form.cleaned_data
             Tweet.objects.create(
                 # figureout username
-                username=data["username"],
+                username=request.user.twitteruser,
                 twang=data["twang"],
-                date=data["date"]
+                # date=data["date"]
             )
-        return HttpResponseRedirect(reverse("homepage"))
+        return HttpResponseRedirect(reverse("home"))
     else:
         form = TwangForm()
     return render(request, html, {"header": header, "form": form,
                                   "button_value": button_value})
+
+
+# own twangs
+def twang_view(request):
+    # get all twangs and render to page
+    html = "twangs.html"
+    twangs = Tweet.objects.all()
+    return render(request, html, {"twangs": twangs})
+
+
+# # own and with followers
+# def author_detail(request, id):
+#     pass
+#     html = "author_detail.html"
+#     authors = Author.objects.all().filter(id=id)
+#     items = Recipes.objects.all()(author_id=id)
+#     return render(request, html, {"authors": authors, "recipes": items})
+    # html = "list_view.html"
+    # items = Recipes.objects.all().order_by("title")
+    # return render(request, html, {"list": items})
