614b48613b9014a3fafbe04076a89fd9b213f9a5
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-16 16:58:54 -0400

    working on getting the date of the tweet to go to that tweet

--- twango/twitteruser/views.py
+++ twango/twitteruser/views.py
@@ -37,33 +37,9 @@ def signup_view(request):
 @login_required()
 def profile_view(request, username):
     html = "../templates/twitteruser.html"
-    # form = None
-    # twitteruser = TwitterUser.objects.filter(username=request.user.username).first()
-    # return render(request, html, {"currentuser": twitteruser})
-    # twitteruser = TwitterUser.objects.filter(user__username=request.user.username).first()
-    # return render(request, html, {"currentuser": twitteruser})
-    # currentuser = TwitterUser.objects.filter(display_name=request.user.display_name).first()
-    # ^^^ this can be helpful when trying to find someone who isnt the loggedin person
     targeteduser = TwitterUser.objects.filter(username=username).first()
-    print(targeteduser)
     targeteduser_twangs = Tweet.objects.filter(user=targeteduser)
-    currentuser = TwitterUser.objects.filter(username=request.user.twitteruser).first()
+    # currentuser = TwitterUser.objects.filter(username=request.user.twitteruser).first()
     # twangs = TwitterUser.objects.all().filter(twitteruser_id=id)
     twangs = Tweet.objects.filter(user=request.user.twitteruser)
     return render(request, html, {"targeteduser": targeteduser, "twangs": targeteduser_twangs})
-
-    # if request.method == "POST":
-    #     form = SignupForm(request.POST)
-    #     if form.is_valid():
-    #         data = form.cleaned_data
-    #         user = User.objects.get(display_name=data["display_name"])
-    #         # # data["username"],data["password"])
-    #         login(request, user)
-    #         TwitterUser.objects.create(
-    #             display_name=data["display_name"],
-    #             user=user
-    #         )
-    #         return User.objects
-    # else:
-    #     form = SignupForm()
-    # return render(request, html, {"header": header, "form": form})
