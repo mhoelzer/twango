b6c25f1d8b479ab99c1725a8a9878bd227d2359f
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-17 11:48:21 -0400

    tweets/twangs function

--- twango/tweet/urls.py
+++ twango/tweet/urls.py
@@ -6,6 +6,6 @@ from twango.tweet.views import (twang_creation_view, twang_view)
 urlpatterns = [
     path("twang/", twang_creation_view),
     path("twangs/", twang_view),
-    path("twang/<int:id>", twang_view)
+    path("twang/<int:id>/", twang_view, name="individual")
     # make sure str woprks
 ]
