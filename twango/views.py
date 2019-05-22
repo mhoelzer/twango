3b9bc8d1465e94b68c8bea8363eb8f82256bed40
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-22 16:12:40 -0400

    filter on homepage shows user and followers

--- twango/views.py
+++ twango/views.py
@@ -8,19 +8,18 @@ from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from twango.tweet.models import Tweet
 from twango.twitteruser.models import TwitterUser
-# from twango.twitteruser.models import TwitterUser
 
 
 @login_required()
 def home_view(request):
     html = "home.html"
-    # make it filter based on following; will have to import w/e
-    # targeteduser = TwitterUser.objects.filter(username=request.user.twitteruser).first()
-    twangs = Tweet.objects.filter(user=request.user.twitteruser)
-    # twangs = Tweet.objects.filter(user=request.user.twitteruser).order_by("-date")
-    twangs_of_followings = Tweet.objects.filter()
+    currentuser = TwitterUser.objects.get(
+        user=request.user)
+    twangs_of_user = Tweet.objects.filter(user=request.user.twitteruser)
+    followers = currentuser.following.all()
+    twangs_of_followings = Tweet.objects.filter(user__in=followers)
+    # if tu in followers, filter;
     # make queries list then pass list; call both then convert; lose o_b w/ list
-    # twangs = Tweet.objects.filter().order_by("-date")
-
-    # twangs = Tweet.objects.filter().order_by("-date")
+    twangs = (twangs_of_followings |
+              twangs_of_user).distinct().order_by("-date")
     return render(request, html, {"twangs": twangs})
